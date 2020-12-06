0f32358b2503ac268f93a1ed898a8158d0a17f6b
Raphael Passos <raphael251@hotmail.com>
2020-12-05 21:44:59 -0300

    chore: add dotenv config in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,26 @@
 import express from 'express';
 import cors from 'cors';
+import { resolve } from 'path';
+import dotenv from 'dotenv';
 import httpRouter from './routes/http';
 
 class App {
   express: express.Application;
 
   constructor() {
+    this.dotEnv();
     this.express = express();
     this.middlewares();
     this.routes();
+  }
+
+  private dotEnv() {
+    dotenv.config({
+      path:
+        process.env.NODE_ENV === 'test'
+          ? resolve(process.cwd(), '.env.test')
+          : resolve(process.cwd(), '.env'),
+    });
   }
 
   private middlewares(): void {
