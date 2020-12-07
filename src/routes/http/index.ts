27075676ada43ad799f00e354dc60d71b9d5aec6
Raphael Passos <raphael251@hotmail.com>
2020-12-06 23:19:50 -0300

    chore: add the orders route to index routes file

--- src/routes/http/index.ts
+++ src/routes/http/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { PipedriveRouter } from './PipedriveRouter';
+import { OrdersRouter } from './OrdersRouter';
 
 const router = Router();
-router.get('/', (req, res) => res.send({ isApiWorking: true }));
+router.get('/', (req, res) => res.send({ isAPIWorking: true }));
 
 router.use(PipedriveRouter);
+router.use(OrdersRouter);
 
 export default router;
